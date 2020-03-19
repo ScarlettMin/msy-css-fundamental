df761862f299133cf8185d93ff19d891e5d834cf
ScarlettMin <salutluvya@gmail.com>
2020-03-19 17:42:07 +0100

    Updates

---
+++ precache-manifest.54c724b55c2ccef726271de86942012b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab2ba59e63cc2b8113dc567cb8e44cc2",
+    "url": "/msy-css-fundamental/index.html"
+  },
+  {
+    "revision": "8c718a13bfba3ccd8263",
+    "url": "/msy-css-fundamental/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5aae87e4ef8f5bae7360",
+    "url": "/msy-css-fundamental/static/js/2.22549bf1.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/msy-css-fundamental/static/js/2.22549bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c718a13bfba3ccd8263",
+    "url": "/msy-css-fundamental/static/js/main.6a663d95.chunk.js"
+  },
+  {
+    "revision": "7b75c03696cc65894b08",
+    "url": "/msy-css-fundamental/static/js/runtime-main.71aa9f62.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/msy-css-fundamental/static/media/logo.5d5d9eef.svg"
+  }
+]);
